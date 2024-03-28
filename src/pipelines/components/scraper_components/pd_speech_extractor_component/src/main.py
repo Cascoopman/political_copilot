46bc655099f63f3e3e4aed448d0dc98451392f15
Cas <cascoopman@hotmail.com>
2024-03-28 12:03:30 +0100

    Added a faction extractor, a write to bucket component and some comments in the code.

--- src/pipelines/components/scraper_components/pd_speech_extractor_component/src/main.py
+++ src/pipelines/components/scraper_components/pd_speech_extractor_component/src/main.py
@@ -3,10 +3,24 @@ import dask.dataframe as dd
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import re
 
 class FetchLinks(DaskLoadComponent):
     def __init__(self, num_pages: int):
         self.pages = num_pages
+        self.regering = {
+            "jan-jambon": "N-VA",
+            "ben-weyts": "N-VA",
+            "zuhal-demir": "N-VA",
+            "matthias-diependaele": "N-VA",
+            "bart-somers": "Open Vld",
+            "lydia-peeters": "Open Vld",
+            "gwendolyn-rutten": "Open Vld",            
+            "benjamin-dalle": "cd&v",
+            "jo-brouns": "cd&v",
+            "wouter-beke": "cd&v",
+            "hilde-crevits": "cd&v",
+        }
     
     def get_pages(self):
         return self.pages
@@ -34,55 +48,112 @@ class FetchLinks(DaskLoadComponent):
         soup = BeautifulSoup(html_content, 'html.parser')
 
         # Find all articles with class "card card--document"
+        # These are the cards that contain the links to all debates
         articles = soup.find_all('article', class_='card card--document')
 
-        article = articles[0]
-        card_title = article.find('h3', class_='card__title').get_text()
-        download_link = article.find('li', class_='card__link card__link-download')
-        doc_number = article.find('span', class_='card__document-number').get_text()
+        # Iterate over each article to extract the debate information
+        for article in articles:
+            card_title = article.find('h3', class_='card__title').get_text()
+            download_link = article.find('li', class_='card__link card__link-download')
+            doc_number = article.find('span', class_='card__document-number').get_text()
 
-        # Extract the text and href attribute from the download link, if it exists
-        if download_link:
-            download_href = download_link.find('a')['href']
-        else:
-            download_href = ''
-
-        card_title = card_title.replace("/", "_") #otherwise it will try to find a folder
-        file_name = f"{card_title}_{doc_number}"
-
-        response = requests.get(download_href)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        speeches = soup.find_all('div', class_="meeting-speech")
-
-        # Iterate over each speech div
-        for speech in speeches:
-            # Extract title
-            title_div = speech.find("div", class_="meeting-speech__title")
-            
-            if title_div:
-                reference = title_div.find('a')
-                if reference:
-                    profile = reference['href']
-                else:
-                    profile = ""
-                title = title_div.text
+            # Extract the text and href attribute from the download link, if it exists
+            if download_link:
+                download_href = download_link.find('a')['href']
             else:
-                profile, title = "", ""
+                download_href = ''
+
+            card_title = card_title.replace("/", "_") #otherwise it will try to find a folder
+            file_name = f"{card_title}_{doc_number}"
+
+            # Fetch the content of the debate
+            response = requests.get(download_href)
+            soup = BeautifulSoup(response.text, 'html.parser')
             
-            text = ""
-            spans = speech.find_all("span")
-            
-            for span in spans:
-                text += " " + span.text
-            
-            data.append({
-                'File Name': file_name,
-                'Card title': card_title, 
-                'Document number': doc_number, 
-                'Download link href': download_href,
-                'Profile': profile,
-                'Title': title,
-                'Text': text
-                })
+            # Find all divs with class "meeting-speech"
+            # These are the blocks of debate text ordered per person
+            speeches = soup.find_all('div', class_="meeting-speech")
+
+            # Iterate over each speech div
+            for speech in speeches:
+                
+                # Extract title
+                title_div = speech.find("div", class_="meeting-speech__title")
+                
+                if title_div:
+                    reference = title_div.find('a')
+                    if reference:
+                        profile = "https://www.vlaamsparlement.be"+reference['href']
+                    else:
+                        profile = ""
+                    title = title_div.text
+                else:
+                    profile, title = "", ""
+                
+                # Drop De voorzitter and empty titles
+                if title == "De voorzitter" or title == "":
+                    continue
+                
+                # Extract which faction the person belongs to
+                pattern = r'\((.*?)\)'
+                matches = re.search(pattern, title)
+                if matches:
+                    faction = matches.group(1)
+                else:
+                    faction = ""
+                
+                # If the faction is not found in the title, the person is part of the regering 2019-2024
+                if faction == "" and profile == "":
+                    name_parts = title.split(' ')[1:]
+                    # Join the name parts with '-'
+                    formatted_name = '-'.join(name_parts).lower()
+                    profile = f"https://www.vlaanderen.be/vlaamse-regering/{formatted_name}"
+                    try:
+                        faction = self.regering[formatted_name]
+                    except KeyError:
+                        print(f"Could not find faction name for {title}, {download_href}")
+                        faction = ""
+                
+                # Some outliers are not part of the regering but their title didn't contain a profile link yet 
+                if profile == "":
+                    name_parts = title.split(' ')[1:]
+                    # Join the name parts with '-'
+                    formatted_name = '-'.join(name_parts).lower()
+                    profile = f"https://www.vlaamsparlement.be/nl/vlaamse-volksvertegenwoordigers-het-vlaams-parlement/{formatted_name}"
+
+                # Some outliers were part of the regering 2019-2024 but did have a profile link in their title
+                if faction == "":
+                    name_parts = title.split(' ')[1:]
+                    # Join the name parts with '-'
+                    formatted_name = '-'.join(name_parts).lower()     
+                    try:
+                        faction = self.regering[formatted_name]
+                    except KeyError:
+                        print(f"Could not find faction name for {title}, {download_href}")
+                        faction = ""
+                
+                # For each speech, extract the text grouped in paragraphs
+                text = ""
+                paragraphs = speech.find_all("p")
+                
+                # Some texts are further nested in a span tag, others are directly in the paragraph
+                for paragraph in paragraphs:
+                    if not paragraph.text:
+                        span = paragraph.find("span")
+                        text += " " + span.text
+                    else:
+                        text += " " + paragraph.text
+                
+                # Append to the database
+                data.append({
+                    'File Name': file_name,
+                    'Card title': card_title, 
+                    'Document number': doc_number, 
+                    'Download link href': download_href,
+                    'Profile': profile,
+                    'Faction': faction,
+                    'Title': title,
+                    'Text': text
+                    })
 
         return data
--- src/pipelines/pd_scraper_pipeline.py
+++ src/pipelines/pd_scraper_pipeline.py
@@ -17,20 +17,38 @@ pipeline = Pipeline(
 speeches = pipeline.read(
     "components/scraper_components/pd_speech_extractor_component",
     arguments={
-        "num_pages": 1,
+        "num_pages": 10,
     }
 )
 
-speeches.write(ref="write_to_file", arguments={"path": "gs://GCPPROJECTNAME _scrape/pv"})
-
-
-#print(f"{GCP_PROJECT_NAME}"+"_scrape/pv")
-#
-# STEP 4: STORE THE DATA IN A GCP BUCKET
-#regex_data.write(
-#    "write_to_file",
+# STEP 3: CHUNK THE DATA INTO SMALLER PARTS
+#dataset = dataset.apply(
+#    "chunk_text",
 #    arguments={
-#        "path": f"gs://{GCP_PROJECT_NAME}_scrape/pv",
-#        "format": "parquet",
-#    }
+        # Add arguments
+        # "chunk_strategy": "RecursiveCharacterTextSplitter",
+        # "chunk_kwargs": {},
+        # "language_text_splitter": ,
+#    },
 #)
+
+# STEP 4: GENERATE EMBEDDINGS
+#dataset = dataset.apply(
+#    "embed_text",
+#    arguments={
+        # Add arguments
+        # "model_provider": "huggingface",
+        # "model": ,
+        # "api_keys": {},
+        # "auth_kwargs": {},
+#    },
+#)
+
+# STEP 5: STORE THE DATA IN A GCP BUCKET
+#print(f"{GCP_PROJECT_NAME}"+"_scrape/pv")
+speeches.write(
+    "write_to_file", 
+    arguments={
+        "path": "gs://GCP_PROJECT_NAME_scrape/pd"
+    }
+)
